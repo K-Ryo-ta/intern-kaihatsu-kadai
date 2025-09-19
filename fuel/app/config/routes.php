cf81fe3b46edd496653cbc38fabbfd248a62e3cd
K-Ryo-ta <studyprograming51@gmail.com>
2025-09-20 03:00:48 +0900

    ルーティングの設定をした。

--- fuel/app/config/routes.php
+++ fuel/app/config/routes.php
@@ -7,4 +7,19 @@ return [
   'portfolios/create' => 'portfolios/create',
   'portfolios/detail/(:num)' => 'portfolios/detail/$1',
   'mypage' => 'mypage/index',
+
+  // API
+  'api/dashboard/items'      => 'api/dashboard/items',
+  'api/dashboard/items.json' => 'api/dashboard/items',
+
+  'api/mypage/profile' => 'api/mypage/profile/index',
+  'api/mypage/portfolio' => 'api/mypage/portfolio/index',
+  'api/mypage/portfolio/(:num)' => 'api/mypage/portfolio/index/$1',
+
+
+  'api/portfolio'           => 'api/portfolio/index',
+  'api/portfolio/detail/(:num)' => 'api/portfolio/detail/$1',
+  'api/portfolio/(:num)'    => 'api/portfolio/index/$1',
+
+  'api/profile'           => 'api/profile/index',
 ];
