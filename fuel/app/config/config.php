77a16227ceda728c0e10075b1bfb7b7c9c26b86d
K-Ryo-ta <studyprograming51@gmail.com>
2025-09-20 03:00:19 +0900

    セキュリティ系の実装をしたcookieとhttp_onlyとsame_site

--- fuel/app/config/config.php
+++ fuel/app/config/config.php
@@ -189,18 +189,13 @@ return array(
   /**
    * Cookie settings
    */
-  // 'cookie' => array(
-  // Number of seconds before the cookie expires
-  // 'expiration'  => 0,
-  // Restrict the path that the cookie is available to
-  // 'path'        => '/',
-  // Restrict the domain that the cookie is available to
-  // 'domain'      => null,
-  // Only transmit cookies over secure connections
-  // 'secure'      => false,
-  // Only transmit cookies over HTTP, disabling Javascript access
-  // 'http_only'   => false,
-  // ),
+
+  //HttpsOnly属性を付与している。
+  'cookie' => array(
+    'secure'      => true,
+    'http_only'   => true,
+    'same_site'    => 'Lax',
+  ),
 
   /**
    * Validation settings
